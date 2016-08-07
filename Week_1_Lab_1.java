520b113befcfc78fe3568cac7e89524277c14d8b
Konstantin Frolov <simplemail84@gmail.com>
2016-08-07 23:42:57 +0100

    ask user to guess number from 1 to 10 in 3 tries

--- Week_1_Lab_1.java
+++ Week_1_Lab_1.java
@@ -11,48 +11,89 @@ import java.util.Scanner;
 public class Week_1_Lab_1 {
 
 	public static void main(String[] args) {
-		
+
 		// Scanner named userInput
 		Scanner userInput = new Scanner(System.in);
-		
+
 		// Initialize variables
 		int randomNumber = 0;
 		int userNumber = 0;
-		
+		int count = -1;
+
 		// Using method Math. every time u run this application var randomNumber == random number from 1 to 10
 		randomNumber = (int)(Math.random() * 10 + 1);
 
 		// Quick explanation to user what to do
-		System.out.println("Try to guess the secret number\n");
+		System.out.println("Try to guess the secret number\nYou have only 3 tries!!!\n");
+
+		// Calling method
+		askUser();
 
 		// Using do-while loop to run it once and depends on result execute or run it again
-		do{
-			// Ask user to input number from 1 to 10
-			System.out.println("Plese input numbers from 1 to 10");	
+		while(randomNumber!= userNumber){
 			
+			// every time it loops variable count adds one number
+			count++;
+
+			// Display how many tries user has left over
+			if(count == 1)
+			{
+				System.out.println("Oh, don't worry you still have 2 more tries to go");
+
+				// Calling method
+				askUser();
+			}
+			else if(count == 2)
+			{
+				System.out.println("Well, try one more time");
+
+				// Calling method
+				askUser();
+			}
+
+			// Display secret number and execute this application
+			else if(count == 3)
+			{
+				System.out.println("The secret number was: " + randomNumber);
+				System.exit(0);
+			}
+
 			// Read user input from keyboard
 			userNumber = userInput.nextInt();
-			
+
 			// Conditions, depends on user input in console will be displayed particular string
-			if(userNumber > randomNumber)
+			if(userNumber > randomNumber && count != 2)
 			{
-				System.out.println("Well it looks like your number is too big,try smaller number");
+				System.out.println("You entered too big number, next time try smaller number");
 			}
-			else if(userNumber < randomNumber)
+			else if(userNumber < randomNumber && count != 2)
 			{
-				System.out.println("Well it looks like your number is too small,try bigger number");
+				System.out.println("You entered too small number, next time try bigger number");
+			}
+			else if(userNumber < randomNumber && count == 2)
+			{
+				System.out.println("Oops, Game Over !\nYou entered too small number");
+			}
+			else if(userNumber > randomNumber && count == 2)
+			{
+				System.out.println("Oops, Game Over !\nYou entered too big number");
 			}
 			else
 			{
 				System.out.println("WoW You Won!!!, the secret number was: " + randomNumber);
 			}
-		
-			// Conditions for loop
-		}while(randomNumber!= userNumber);
+
+		}
 
 		// Always close the scanner  
 		userInput.close();
 
+	}
+
+	private static void askUser()
+	{
+		// Ask user to input number from 1 to 10
+		System.out.println("Plese input numbers from 1 to 10");	
 	}
 
 }
